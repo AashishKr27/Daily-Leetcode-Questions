36e57d0a6756385e8555e81238171f80934e54d6
Ashish Kumar <147747648+AashishKr27@users.noreply.github.com>
2024-02-14 00:14:26 +0530

    Product_of_array_except_self.cpp

--- Product_of_array_except_self/Product_of_array_except_self.cpp
+++ Product_of_array_except_self/Product_of_array_except_self.cpp
@@ -1,3 +1,27 @@
+/*
+# Product-of-array-except-self
+
+# DESCRIPTION :
+    You are given an integer array nums. You need to return an array arr,
+    such that arr[i] = product of all elements in the array except nums[i] itself.
+    
+# APROACH : 
+    STEP 1 : INITIALIZATION : "(n -> length of the given array nums)"
+                -> Initialize two arrays left and right of length n, to compute the left and right
+                    product of each element the array. Initialize left[0] = 1 and right[n-1] = 1;
+                -> Iterate nums from 1 to n and store the result by using relation left[i] = left[i-1] * nums[i-1];
+                -> Iterate nums from n-2 to 0 and store the result by using relation right[i] = right[i+1] * nums[i+1];
+    STEP 2 : COMPUTE FINAL RESULT :
+                -> You can either use a new array of length n to store the result, or can use
+                    the existing array nums to optimize the space complexity a little bit.
+                -> For each index i in nums, nums[i] = left[i] * right[i];
+    STEP 3 : RETURN FINAL RESULT :
+                -> Return nums.
+                
+# TIME & SPACE COMPLEXITY : 
+    -> The code has a worst case time complexity : O(n).
+    -> The code has a space complexity : O(n).
+*/
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
